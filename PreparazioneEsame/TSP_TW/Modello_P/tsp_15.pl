aeaba96fed836b1c350587eef6ba54c8da443dad
Dominik Miotla <dominik.miotla@edu.unife.it>
2025-06-03 10:10:42 +0200

    TSP_TW Modello_P Propia

---
+++ PreparazioneEsame/TSP_TW/Modello_P/tsp_15.pl
@@ -0,0 +1,40 @@
+:- [pazienti15].
+:- lib(fd).
+:- lib(propia).
+:- lib(fd_global).
+
+tsp(L):-
+    generate_variables(Domain,Dim),
+    length(L,Dim),
+    L :: Domain,
+
+    impose_start(L),
+    fd_global:alldifferent(L),
+
+    route(L,0,Cost),
+
+    append(L,[Cost],Vars),
+    minimize(labeling(Vars),Cost).
+
+
+
+route([A],Acc,Cost):-!,
+    distanza(A,0,C) infers most,
+    Cost #= Acc + C.
+route([A,B|T],Acc,Cost):-
+    distanza(A,B,C) infers most,
+    paziente(B,Min,Max) infers most,
+    Time #= Acc + C,
+    Time #>= Min,
+    Time #<= Max,
+    route([B|T],Time,Cost).
+
+
+impose_start([H|_]):-
+    H #= 0.
+
+
+generate_variables(Domain,Dim):-
+    findall(ID, paziente(ID,_,_),ListID),
+    append(ListID,[0],Domain),
+    length(Domain,Dim).
