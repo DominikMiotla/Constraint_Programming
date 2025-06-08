9da7fa0074169a631e0332944fe61ced4d5bdba7
Dominik Miotla <dominik.miotla@edu.unife.it>
2025-06-08 16:23:59 +0200

    PE_18_09_2017

---
+++ Testi_Esame/PE_18_09_2017/sposi.pl
@@ -0,0 +1,54 @@
+:- [matrimonio].
+:- lib(fd).
+:- lib(listut).
+:- lib(fd_global).
+
+sposi(L):-
+    read_data(NI,NT,C,ListConflict,ListFriends),
+    length(L,NI),
+    L :: [1..NT],
+    DomainC :: [1..C],
+
+    constraint_conflict(ListConflict,L),
+    fd_global:alldifferent(L,C),
+    %constraint_capacity(1,NT,L,DomainC),
+
+    objective(ListFriends,L,ListC),
+    fd_global:sumlist(ListC,Sum),
+    Obj #= -Sum,
+    append(L,[DomainC],Vars),
+    minimize(labeling(Vars),Obj).
+
+
+
+
+objective([],_,[]).
+objective([conosce(ID1,ID2)|ListFriends],L,[C|ListC]):-
+    nth1(ID1,L,A),
+    nth1(ID2,L,B),
+    #=(A,B,C),
+    objective(ListFriends,L,ListC).
+
+
+
+constraint_capacity(NT,NT,L,DomainC):-!,
+    occurrences(NT,L,DomainC).
+constraint_capacity(N,NT,L,DomainC):-
+    occurrences(N,L,DomainC),
+    N1 is N + 1,
+    constraint_capacity(N1,NT,L,DomainC).
+
+constraint_conflict([],_).
+constraint_conflict([conflitto(ID1,ID2)|ListConflict],L):-
+    nth1(ID1,L,A),
+    nth1(ID2,L,B),
+    A #\= B,
+    constraint_conflict(ListConflict,L).
+
+
+read_data(NI,NT,C,ListConflict,ListFriends):-
+    num_invitati(NI),
+    num_tavoli(NT),
+    capacita(C),
+    findall(conflitto(ID1,ID2),conflitto(ID1,ID2),ListConflict),
+    findall(conosce(ID1,ID2),conosce(ID1,ID2),ListFriends).
